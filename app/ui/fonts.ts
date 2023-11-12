2ea9aba7cff68f9b8e6b5f9824c54e84389fe750
venom-10 <arohit0102@gmail.com>
2023-11-12 12:39:55 +0530

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Lusitana } from "next/font/google";
+
+export const lusit = Lusitana({ weight: "700", subsets: ["latin"] });
+export const poppins = Poppins({ subsets: ["devanagari"], weight: "400" });
